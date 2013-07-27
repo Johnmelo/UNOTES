5e671ce84ca061e76aa833501d7672cf132d1fe4
Jonathas Melo <jonathas_ruam@hotmail.com>
2013-07-26 22:54:19 -0300

    Iniciando projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UNOTES::Application.config.secret_token = '3913491287c621e0e8d15b811a0c224e9369fdb08c3a4f8255b00451d412f3ffea6b70a50fc20c6c460ec930861fb8c1068dc0a3e92138530f662fd23f6bb141'
