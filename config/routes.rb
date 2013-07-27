5c568e5ba8be6f5e0d8b29c26e8c4b34e1bc522d
Jonathas Melo <jonathas_ruam@hotmail.com>
2013-07-27 00:12:37 -0300

    Instalando devise, Criando controller e views do users;Criando Controller e view Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 UNOTES::Application.routes.draw do
+  devise_for :users
+
+  resources :users
+
   resources :grupos
 
 
@@ -51,7 +55,7 @@ UNOTES::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
